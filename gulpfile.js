3077678b802a3a10d2e985e3812d776cbbb38e15
Joel Lachance <jlsubdesign@gmail.com>
2018-09-05 12:32:34 -0400

    Page inscription

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 var elixir = require('laravel-elixir');
 
 elixir.config.assetsPath = 'themes/cifq/assets/';
-elixir.config.publicPath = 'themes/cifq/assets/compiled/';
+elixir.config.publicPath = 'themes/cifq/assets/public/';
 
 elixir (function(mix){
    mix.sass([
      'app.scss'
    ], 'themes/cifq/assets/compiled/app.css', 'themes/cifq/assets/src/stylesheets');
+
+   mix.sass([
+    'foret.scss'
+  ], 'themes/cifq/assets/compiled/foret.css', 'themes/cifq/assets/src/stylesheets');
 
 })
